519c5d03fd17de41d06606753fcf5329e50bb86a
David Goldwasser <david.goldwasser@nrel.gov>
2020-02-28 12:13:17 -0700

    adding remove orphan meausre from OpenStudio-results

---
+++ lib/measures/remove_orphan_objects_and_unused_resources/tests/remove_orphan_objects_and_unused_resources_test.rb
@@ -0,0 +1,111 @@
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) 2008-2018, Alliance for Sustainable Energy, LLC.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# (1) Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# (2) Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# (3) Neither the name of the copyright holder nor the names of any contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission from the respective party.
+#
+# (4) Other than as required in clauses (1) and (2), distributions in any form
+# of modifications or other derivative works may not use the "OpenStudio"
+# trademark, "OS", "os", or any other confusingly similar designation without
+# specific prior written permission from Alliance for Sustainable Energy, LLC.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE
+# UNITED STATES GOVERNMENT, OR THE UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF
+# THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# *******************************************************************************
+
+require 'openstudio'
+require 'openstudio/measure/ShowRunnerOutput'
+require 'fileutils'
+
+require_relative '../measure.rb'
+require 'minitest/autorun'
+
+class RemoveOrphanObjectsAndUnusedResourcesTest < Minitest::Test
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_number_of_arguments_and_argument_names
+    # create an instance of the measure
+    measure = RemoveOrphanObjectsAndUnusedResources.new
+
+    # make an empty model
+    model = OpenStudio::Model::Model.new
+
+    # get arguments and test that they are what we are expecting
+    arguments = measure.arguments(model)
+    assert_equal(5, arguments.size)
+  end
+
+  def test_good_argument_values
+    # create an instance of the measure
+    measure = RemoveOrphanObjectsAndUnusedResources.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = OpenStudio::Path.new(File.dirname(__FILE__) + '/1125_infil_test_a.osm')
+    model = translator.loadModel(path)
+    assert(!model.empty?)
+    model = model.get
+
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+
+    # set argument values to good values
+    remove_unused_space_types = arguments[0].clone
+    assert(remove_unused_space_types.setValue(true))
+    argument_map['remove_unused_space_types'] = remove_unused_space_types
+
+    remove_unused_load_defs = arguments[1].clone
+    assert(remove_unused_load_defs.setValue(true))
+    argument_map['remove_unused_load_defs'] = remove_unused_load_defs
+
+    remove_unused_schedules = arguments[2].clone
+    assert(remove_unused_schedules.setValue(true))
+    argument_map['remove_unused_schedules'] = remove_unused_schedules
+
+    remove_unused_curves = arguments[3].clone
+    assert(remove_unused_curves.setValue(true))
+    argument_map['remove_unused_curves'] = remove_unused_curves
+    
+    remove_unused_constructions = arguments[4].clone
+    assert(remove_unused_constructions.setValue(true))
+    argument_map['remove_unused_constructions'] = remove_unused_constructions
+
+    # run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+    assert_equal('Success', result.value.valueName)
+
+    # save the model
+    output_file_path = OpenStudio::Path.new(File.dirname(__FILE__) + '/output/test.osm')
+    model.save(output_file_path, true)
+  end
+end
