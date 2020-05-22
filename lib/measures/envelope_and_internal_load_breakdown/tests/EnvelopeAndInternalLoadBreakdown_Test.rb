ca0dabab4ae0d334da5b321e21bd36c7c9a69f0b
David Goldwasser <david.goldwasser@nrel.gov>
2020-05-22 14:04:46 -0600

    This will now catch section error
    Had been copied from qaqc measure without updating test string. It now catches a section error for this measure.

--- lib/measures/envelope_and_internal_load_breakdown/tests/EnvelopeAndInternalLoadBreakdown_Test.rb
+++ lib/measures/envelope_and_internal_load_breakdown/tests/EnvelopeAndInternalLoadBreakdown_Test.rb
@@ -167,7 +167,7 @@ class EnvelopeAndInternalLoadBreakdown_Test < MiniTest::Unit::TestCase
 
   # assert that no section errors were thrown
   def section_errors(runner)
-    test_string = 'Error prevented QAQC check from running'
+    test_string = 'section failed and was skipped because'
 
     if is_openstudio_2?
       section_errors = []
